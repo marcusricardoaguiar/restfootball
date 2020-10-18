c3df594f136156855774f5e7403cc392f83cb57b
Marcus Ricardo Aguiar dos Santos <CIANDT\marcussantos@lnb020154bhz.ciandt.global>
2020-10-18 18:53:56 -0300

    Add README, Swagger Configuration and Postman

--- src/main/java/de/planerio/developertest/SwaggerConfig.java
+++ src/main/java/de/planerio/developertest/SwaggerConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -17,8 +20,9 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis( RequestHandlerSelectors.basePackage("de.planerio.developertest"))
                 .paths(PathSelectors.any())
                 .build();
     }
